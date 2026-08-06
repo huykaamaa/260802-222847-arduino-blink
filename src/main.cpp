005ee22869303328d5cc355bfdf6357c97149ce7
huykaamaa <iwbrhwfy@gmail.com>
2026-08-06 15:54:46 +0700

    doi chan

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@
 // ======================================================================
 // PIN CONFIG
 // ======================================================================
-const uint8_t sensorPins[SENSOR_NUM] = {40, 39};
+const uint8_t sensorPins[SENSOR_NUM] = {39, 40};
 // 3 cap: cap0=relay1+2 (4,5), cap1=relay3+4 (6,16), cap2=relay5+6 (15,7). Xem RELAY_PAIR_NUM
 // trong globals.h.
 const uint8_t relayPins[RELAY_NUM] = {4, 5, 6, 16, 15, 7};
